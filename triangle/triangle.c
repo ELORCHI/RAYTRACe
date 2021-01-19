de25b31bea4b451d5e8e387ab93db4c985cea5c1
ELORCHI <elorchie@gmail.com>
2021-01-20 00:32:17 +0100

    triangle

--- triangle/triangle.c
+++ triangle/triangle.c
@@ -14,6 +14,14 @@
 
 t_intersection ray_triangles_intersections(t_ray ray, t_triangle *triangle)
 {
+    t_vector v1;
+    t_vector v2;
+    t_vector v3;
+
+    v1 = vector_point(triangle->p2, triangle->p1); 
+    v2 = vector_point(triangle->p3, triangle->p1);
+    triangle->normal = normaliz(cross_product(v1, v2));
+    /// READ THE NOTEED ARTICEL AGAIN TO FINISH THE IMPLEMENTATION
 	// calulate two vectors AC AB using  the triangle vertices
 	// then caluclate the normal using the cross product if these
 	// test of the ray intersect the defined by the calculeted normal
