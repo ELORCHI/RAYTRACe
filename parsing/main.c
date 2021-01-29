205cfd5249a667c3fe0e1a2f333d314bb6fa474e
ELORCHI <elorchie@gmail.com>
2021-01-29 02:46:22 +0100

    pars resolution

--- parsing/main.c
+++ parsing/main.c
@@ -1,20 +1,51 @@
-int init_world(char **param)
+bool	check_resolution(int hsize, int vsize)
 {
-	while (param != NULL)
+	if (hsize < 0 || vsize < 0)
 	{
-			
+		//("ERROR IN RESOLUTION\nresolution must be positive");
+		return (false);
 	}
 }
 
-int parssing(int fd, t_world world)
+bool	is_digit(char *line)
 {
-	char 	*line;
-	char	**param;
-	int		i;
+	int i;
 
-	line = "";
-	i = get_next_line(fd, &line);
-	param = ft_split(line, '32', '	');
-	free(line);
-	init_world(param);
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] >= 48 && line[i] <= 57)
+			i++;
+		else
+			return (false)
+	}
+	return (true);
+}
+
+void	get_resolution(char **line)
+{
+	int hsize;
+	int	vsize;
+
+	if (line != NULL)
+	{
+		if (!is_digit(*line))
+		{
+			//("RESOLUTION ERROR\nonly numbers are allowed");
+			//EXIT_FALUIRE
+		}
+		hsize = ft_atoi(line[1]);
+	}
+	if (++line != NULL)
+	{		
+		if (!is_digit(*line))
+		{
+			//("RESOLUTION ERROR\nonly numbers are allowed");
+			//EXIT_FALUIRE
+		}
+		vsize = ft_atoi(line[2]);
+	}
+	if (++line != NULL)
+		//("RESOLUTION ERROR\nerror to much params for resolution");
+	return (check_resolution(hsize, vsize));
 }
