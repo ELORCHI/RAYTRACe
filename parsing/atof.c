23cfda9ae04feee469c6c9234e05801218dce8f8
ELORCHI <elorchie@gmail.com>
2021-02-06 01:19:19 +0100

    i think its working

--- parsing/atof.c
+++ parsing/atof.c
@@ -1,14 +1,21 @@
 #include "./libft/libft.h"
 #include "parsing.h"
 
-float	to_float(int int_part, int decimal_part, int negative)
+float	to_float(int int_part, char *decimal, int negative)
 {
+	int		decimal_part;
 	int 	decimal_len;
 	int 	tmp;
 	float	number;
+	int		i;
 
+	i = 0;
+	decimal_len = 0;
 	tmp = int_part;
-	decimal_len = ft_int_size(decimal_part);
+	while (decimal[i] == 48)
+		i++;
+	decimal_part = ft_atoi(decimal + i);
+	decimal_len = ft_int_size(decimal_part) + i;
 	int_part = int_part * pow(10, decimal_len);
 	if (negative == 1 && int_part != 0)
 		decimal_part = -1 * decimal_part;
@@ -22,38 +29,36 @@ float	to_float(int int_part, int decimal_part, int negative)
 float	ft_atof(char *line)
 {
 	int 	int_part;
-	int 	decimal_part;
 	int 	int_len;
 	int		negative;
 	float	number;
+	int		i;
 
+	i = 0;
 	int_len = 0;
 	negative = 0;
-	if (line[0] == '-' && line[1] == '0')
+	if (line[i] == '-')
 	{
+		i = 1;
 		negative = 1;
-		int_len = 1;
 	}
-	printf("negative |%d|\n",negative);
+	while (line[i] == 48)
+		i++;
 	int_part = ft_atoi(line);
-	printf("int_part==%d\n", int_part);
-	negative = (int_part < 0) ? 1 : negative;
 	int_len += ft_int_size(int_part);
-	printf("int_len==%d\n", int_len);
-	decimal_part = ft_atoi(line + int_len + 1);
-	printf("int_part == %d\ndecimal_part == %d\n is_negative == %d\n", int_part, decimal_part, negative);
-	number = to_float(int_part, decimal_part, negative);
+	int_len += (i > 1) ? --i : 0;
+	number = to_float(int_part, line + int_len + 1, negative);
 	return(number); 
 }
 
 int main()
 {
 	float decimal;
-	char *line = "222,2";
+	char *line = "-69.0063";
 	int test;
 
 	test = ft_atoi(line);
 	decimal = ft_atof(line);
-	printf("%f",decimal);
+	printf("%f\n%d\n",decimal, test);
 	return (0);
 }
