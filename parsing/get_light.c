ef761af32282381cf7edf2c965badc0d46326c4a
ELORCHI <elorchie@gmail.com>
2021-02-10 17:51:52 +0100

    rm debuging shit

--- parsing/get_light.c
+++ parsing/get_light.c
@@ -29,44 +29,37 @@ void	get_light(char **params)
 	get_color(*params, &(tmp->color));
 	tmp->next = NULL;
 	if (i == 0)
-	{
 		g_light = tmp;
-		printf("%d\n", i);
-	}
 	else
 	{
 		pars = g_light;
-		printf("%d\n", i);
 		while (pars->next != NULL)
-		{
 			pars = pars->next;
-			printf("3\n");
-		}
 		pars->next = tmp;
 		i++;
 	}
 	i++;
 }
 
-int main()
-{
-	char *line1 = ft_strdup("l 0.2,10.7,9 0.6 255,0,255");
-	char *line2 = ft_strdup("l 8,10.7,9 0.6 255,0,33");
-	line1 = skip_tabs(&line1);
-	line2 = skip_tabs(&line2);
-	char **params1 = ft_split(line1, 32);
-	char **params2 = ft_split(line2, 32);
-	get_light(params1);
-	get_light(params2);
-	// while (g_light->next != NULL)
-	// {
-	//  	print_vector(g_light->orig);
-	// 	printf("light ratio == %f\n", g_light->ratio);
-	// 	print_vector(g_light->color);
-	// 	g_light = g_light->next;
-	// }
-	// print_vector(g_light->orig);
-	// printf("light ratio == %f\n", g_light->ratio);
-	// print_vector(g_light->color);
-	return (0);
-}
+// int main()
+// {
+// 	char *line1 = ft_strdup("l 0.2,10.7,9 0.6 255,0,255");
+// 	char *line2 = ft_strdup("l 8,10.7,9 0.6 255,0,33");
+// 	line1 = skip_tabs(&line1);
+// 	line2 = skip_tabs(&line2);
+// 	char **params1 = ft_split(line1, 32);
+// 	char **params2 = ft_split(line2, 32);
+// 	get_light(params1);
+// 	get_light(params2);
+// 	// while (g_light->next != NULL)
+// 	// {
+// 	//  	print_vector(g_light->orig);
+// 	// 	printf("light ratio == %f\n", g_light->ratio);
+// 	// 	print_vector(g_light->color);
+// 	// 	g_light = g_light->next;
+// 	// }
+// 	// print_vector(g_light->orig);
+// 	// printf("light ratio == %f\n", g_light->ratio);
+// 	// print_vector(g_light->color);
+// 	return (0);
+// }
