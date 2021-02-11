e3c350ce2342df86bc5ff3c03cceb56bcada40dd
ELORCHI <elorchie@gmail.com>
2021-02-11 09:59:13 +0100

    get_squares tested well

--- parsing/get_squares.c
+++ parsing/get_squares.c
@@ -15,8 +15,9 @@
 //not tested yet
 void get_squares(t_square **square, char **params)
 {
-	t_square *tmp;
-	t_square *pars;
+	t_square 	*tmp;
+	t_square 	*pars;
+	static int	i = 0;
 
 	count_params(params, 5);
 	tmp = (t_square *)malloc(sizeof(t_square));
@@ -26,18 +27,44 @@ void get_squares(t_square **square, char **params)
 	params++;
 	tmp->normal = get_vector(*params);
 	params++;
-	get_scalar(params, &(tmp->side));
+	get_scalar(*params, &(tmp->side));
 	if (tmp->side < 0)
 		ft_exit("ERROR\n square side must be positive");
 	params++;
-	get_color(params ,&tmp->color);
-	if ((*square)->next == NULL)
+	get_color(*params ,&(tmp->color));
+	if (i == 0)
 		(*square)= tmp;
 	else
 	{
-		pars = (*square)->next;
-		while (pars->next!= NULL)
+		pars = (*square);
+		while (pars->next != NULL)
 			pars = pars->next;
 		pars->next = tmp;
 	}
+	i = 1;
+}
+
+int main ()
+{
+	char *line1 = ft_strdup("sq -10,-10,-10 0,0,1.0 30 42,42,0");
+	char *line2 = ft_strdup("sq 100,100,40 10,0,1.0 5 0,30,0");
+
+	line1 = skip_tabs(&line1);
+	line2 = skip_tabs(&line2);
+
+	char **params1 = ft_split(line1, 32);
+	char **params2 = ft_split(line2, 32);
+
+	t_square *square;
+	get_squares(&square, params1);
+	get_squares(&square, params2);
+	while (square != NULL)
+	{
+		print_vector(square->center);
+		print_vector(square->normal);
+		printf("side = %f\n",square->side);
+		print_vector(square->color);
+		square = square->next;
+	}
+	return (0);
 }
