7a15e4e00398ab196b5c3df489a309cb929fb496
ELORCHI <elorchie@gmail.com>
2021-02-12 18:16:42 +0100

    square working correctly

--- square/square.c
+++ square/square.c
@@ -53,6 +53,15 @@ bool        try_sq(t_ray *ray, t_square *sq, double *t)
 	printf("%f\n", p.x);
 	return (fabs(p.x) <= mx && fabs(p.y) <= mx);
 }*/
+
+float			dot(t_vector vec1, t_vector vec2)
+{
+	float dot;
+
+	dot  = vec1.x * vec2.x + vec1.y * vec2.y +  vec1.z * vec2.z;
+	return (dot);
+}
+
 bool			inside_square(t_square *square, t_vector intersection)
 {
 	t_vector 	width;
@@ -62,13 +71,18 @@ bool			inside_square(t_square *square, t_vector intersection)
 	float		proj_hight;
 
 	hight = normaliz(cross_product((t_vector){0, 1, 0, 0}, square->normal));
+	
+	if (hight.x == 0 && hight.y == 0 && hight.z == 0)
+		hight = normaliz(cross_product((t_vector){1, 0, 0, 0}, square->normal));
+	
 	width = normaliz(cross_product(square->normal, hight));
 	center_to_inter = point_vector(square->center, intersection);
-	proj_hight = dot_product(hight, center_to_inter);
+	
+	proj_hight = dot(hight, center_to_inter);
 	proj_width = dot_product(width, center_to_inter);
-	if ((fabs(proj_width) < (square->side / 2)) && (fabs(proj_hight) < (square->side / 2)))
-		return (true);
-	return (false);
+	if ((fabs(proj_width) > (square->side / 2)) || (fabs(proj_hight) > (square->side / 2)))
+		return (false);
+	return (true);
 }
 
 t_plan			plan_sqaure(t_square *square)
@@ -81,21 +95,51 @@ t_plan			plan_sqaure(t_square *square)
 	return (plan);
 }
 
+// t_intersection	ray_sqaures_intersection(t_square *square, t_ray ray)
+// {
+//     t_intersection 	inter_plan;
+// 	t_plan			*plan;
+// 	t_square		*tmp_square;
+// 	t_intersection	inter;
+
+// 	inter.hit = FLT_MAX;
+// 	tmp_square = square;
+// 	plan = (t_plan *)malloc(sizeof(t_plan));
+// 	while (square != NULL)
+// 	{
+// 		*plan = plan_sqaure(square);
+// 		plan->next = NULL;
+// 		inter_plan = ray_plans_intersection(plan, ray);
+// 		//printf("%f\n",inter_plan.hit);
+// 		if (inter_plan.hit != -1 && inter_plan.hit < inter.hit)
+// 		{
+// 			if (inside_square(square, inter_plan.point) == true)
+// 			{
+// 				inter = inter_plan;
+// 				//printf("inside && hit == %f", inter.hit);
+// 			}
+// 		}
+// 		square = square->next; 
+// 	}
+// 	free(plan);
+// 	if (inter.hit == FLT_MAX)
+// 		inter.hit = -1;
+// 	return (inter);
+// }
+
 t_intersection	ray_sqaures_intersection(t_square *square, t_ray ray)
 {
     t_intersection 	inter;
-	t_plan			plan;
+	t_plan			*plan;
 	bool			is_inside;
 
-	plan = plan_sqaure(square);
-	inter = ray_plans_intersection(&plan, ray);
-	//printf("%\n", inter.hit);
+	plan = (t_plan *)malloc(sizeof(t_plan));
+	*plan = plan_sqaure(square);
+	plan->next = NULL;
+	inter = ray_plans_intersection(plan, ray);
 	if (inter.hit != -1)
 	{
 		is_inside = inside_square(square, inter.point);
-		if (is_inside)
-			printf("ufvfbvf");
-		// print
 		if (!is_inside)
 			inter.hit = -1;
 	}
