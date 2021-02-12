887201a65d12d9cf2735075378c394cc4cf43497
ELORCHI <elorchie@gmail.com>
2021-02-12 12:57:10 +0100

    intersecting multiple triangle tested well

--- triangle/tr.c
+++ triangle/tr.c
@@ -3,43 +3,82 @@
 #include "../include/plan.h"
 
 
-t_intersection	check(t_vertex vertex, t_intersection inter, t_ray ray, t_triangle tr)
+void	check(t_vertex vertex, t_intersection *inter, t_ray ray, t_triangle *tr)
 {
-	if (inter.hit > EPSILON)
+	static float 	near = FLT_MAX;
+	static int		i = 0;
+
+	near = FLT_MAX;
+	if (inter->hit > EPSILON && inter->hit < near)
 	{
-		inter.point = position(ray, inter.hit);
-		inter.color = tr.color;
-		inter.normal = cross_product(vertex.edge2, vertex.edge1);
-		//fix_normal_for_multpl lights
-		return(inter);
+		inter->point = position(ray, inter->hit);
+		inter->color = tr->color;
+		inter->normal = cross_product(vertex.edge2, vertex.edge1);
+		near = inter->hit;
 	}
-	inter.hit = -1;
-	return (inter);
+	if (i == 1)
+		near = FLT_MAX;
+	if (tr->next == NULL)
+		i = 1;
+}
+
+t_vertex	helper(t_triangle *tr, t_vector *cross, float *dot, t_ray ray)
+{
+	t_vertex vertex;
+
+	vertex.edge1 = point_vector(tr->p1, tr->p2);
+	vertex.edge2 = point_vector(tr->p1, tr->p3);
+	*cross = cross_product(ray.dir, vertex.edge2);
+	*dot = dot_product(vertex.edge1, *cross);
+	return (vertex);
+}
+
+int				for_normintte(t_vertex *vertex, t_vector *cross, t_ray ray, float *dot)
+{
+
+	vertex->u = (*dot) * dot_product(vertex->ray_to_tr, *cross);
+	if (vertex->u < 0.0 || vertex->u > 1.0)
+				return 0;
+	*cross = cross_product(vertex->ray_to_tr, vertex->edge1);
+	vertex->v = (*dot) * dot_product(ray.dir, *cross);
+	if (vertex->v < 0.0 || (vertex->v + vertex->u > 1.0))
+		return (0);
+	return (1);
 }
 
 t_intersection	ray_triangles_intersections(t_ray ray, t_triangle *triangle)
 {
 	t_vertex	 	vertex;
 	t_vector	 	cross;
-	t_intersection inter;
+	t_intersection 	inter;
+	t_triangle		*tmp_tr;
 	float			dot;
 
+	tmp_tr = triangle;
 	inter.hit = -1;
-	vertex.edge1 = point_vector(triangle->p1, triangle->p2);
-	vertex.edge2 = point_vector(triangle->p1, triangle->p3);
-	cross = cross_product(ray.dir, vertex.edge2);
-	dot = dot_product(vertex.edge1, cross);
-	if (dot > -EPSILON && dot < EPSILON) 
-		return (inter);
-	dot = 1.0 / dot;
-	vertex.ray_to_tr = point_vector(triangle->p1, ray.orig);
-	vertex.u = dot * dot_product(vertex.ray_to_tr, cross);
-	if (vertex.u < 0.0 || vertex.u > 1.0)
-		return (inter);
-	cross = cross_product(vertex.ray_to_tr, vertex.edge1);
-	vertex.v = dot * dot_product(ray.dir, cross);
-	if (vertex.v < 0.0 || (vertex.v + vertex.u > 1.0))
-		return (inter);
-	inter.hit = dot * dot_product(cross, vertex.edge2);
-	return (check(vertex, inter, ray, *triangle));
+	while (tmp_tr != NULL)
+	{
+		while (1)
+		{
+			vertex = helper(tmp_tr, &cross, &dot, ray);
+			if (dot > -EPSILON && dot < EPSILON) 
+				break;
+			dot = 1.0 / dot;
+			vertex.ray_to_tr = point_vector(tmp_tr->p1, ray.orig);
+			// vertex.u = dot * dot_product(vertex.ray_to_tr, cross);
+			// if (vertex.u < 0.0 || vertex.u > 1.0)
+			// 	break;
+			// cross = cross_product(vertex.ray_to_tr, vertex.edge1);
+			// vertex.v = dot * dot_product(ray.dir, cross);
+			// if (vertex.v < 0.0 || (vertex.v + vertex.u > 1.0))
+			// 	break;
+			if (for_normintte(&vertex, &cross, ray, &dot) == 0)
+				break;
+			inter.hit = dot * dot_product(cross, vertex.edge2);
+			check(vertex, &inter, ray, tmp_tr);
+			break;
+		}
+		tmp_tr = tmp_tr->next;
+	}
+	return (inter);
 }
