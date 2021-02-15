aba12a30ea3bb415e83886bb990f354aa5b6e941
ELORCHI <elorchie@gmail.com>
2021-02-15 10:48:25 +0100

    free sipmle objects

--- parsing/free.c
+++ parsing/free.c
@@ -1,4 +1,4 @@
-#include "parssing.h"
+#include "parsing.h"
 
 // not tested yet
 void	free_spheres(t_sphere **sphere)
@@ -13,15 +13,65 @@ void	free_spheres(t_sphere **sphere)
 	}
 }
 
-//if this is working no need to write a fucntion fot every object
-void	free_objects(void **object)
-{
-	void *tmp_object;
+//this  is not working
+// void	free_objects(void **object)
+// {
+// 	void *tmp_object;
+// 	void *tmp_next;
 
-	while (*object)
+// 	while (*object)
+// 	{
+// 		tmp_object = *object;
+// 		tmp_next = (void *)(*object)->((void *)next)
+// 		*object = tmp_next;
+// 		free(tmp_object);
+// 	}
+// }
+
+void	free_triangles(t_triangle **tr)
+{
+	t_triangle *tmp_tr;
+
+	while (*tr)
 	{
-		tmp_object = *object;
-		*object = (*object)->next;
-		free(tmp_object);
+		tmp_tr = *tr;
+		*tr = (*tr)->next;
+		free(tmp_tr);
+	}
+}
+
+void	free_cylinders(t_cylinder **cylinder)
+{
+	t_cylinder *tmp_cy;
+
+	while (*cylinder)
+	{
+		tmp_cy = *cylinder;
+		*cylinder = (*cylinder)->next;
+		free(tmp_cy);
+	}
+}
+
+void	free_squares(t_square **square)
+{
+	t_square *tmp_sq;
+
+	while (*square)
+	{
+		tmp_sq = *square;
+		*square = (*square)->next;
+		free(tmp_sq);
+	}
+}
+
+void	free_planes(t_plan **plan)
+{
+	t_plan *tmp_plan;
+
+	while (*plan)
+	{
+		tmp_plan = *plan;
+		*plan = (*plan)->next;
+		free(tmp_plan);
 	}
 }
