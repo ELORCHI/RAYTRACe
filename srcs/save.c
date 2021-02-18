0ba6a2e9224857e5bcacb575f82ddba3ef7ca9c4
ELORCHI <elorchie@gmail.com>
2021-02-18 09:33:58 +0100

    saving working correctly

--- srcs/save.c
+++ srcs/save.c
@@ -17,7 +17,7 @@ void	bmp_initialize(t_bmp *bmp)
 	int i;
 	i = 0;
 	bmp->bitcount = 32;
-	bmp->imagesize = g_resolution.vsize * g_resolution.hsize * 4;
+	bmp->imagesize = g_resolution.hsize * g_resolution.vsize * 4;
 	bmp->bisize = 40;
 	bmp->bfoff_bits = 54;
 	bmp->filesize = 54 + bmp->imagesize;
@@ -28,8 +28,8 @@ void	bmp_initialize(t_bmp *bmp)
 	ft_memcpy(bmp->header + 2, &bmp->filesize, 4);
 	ft_memcpy(bmp->header + 10, &bmp->bfoff_bits, 4);
 	ft_memcpy(bmp->header + 14, &bmp->bisize, 4);
-	ft_memcpy(bmp->header + 18, &g_resolution.vsize, 4);
-	ft_memcpy(bmp->header + 22, &g_resolution.hsize, 4);
+	ft_memcpy(bmp->header + 18, &g_resolution.hsize, 4);
+	ft_memcpy(bmp->header + 22, &g_resolution.vsize, 4);
 	ft_memcpy(bmp->header + 26, &bmp->bi_planes, 2);
 	ft_memcpy(bmp->header + 28, &bmp->bitcount, 2);
 	ft_memcpy(bmp->header + 34, &bmp->imagesize, 4);
@@ -44,15 +44,15 @@ void	create_bmp_file(void)
 	int				*pixel;
 	i = 0;
 	bmp_initialize(&bmp);
-	j = g_resolution.hsize - 1;
+	j = g_resolution.vsize - 1;
 	bmp.fd = open("minirt.bmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	write(bmp.fd, bmp.header, 54);
 	while (j > -1)
 	{
 		i = 0;
-		while (i < g_resolution.vsize)
+		while (i < g_resolution.hsize)
 		{
-			bmp.pos = (i + g_resolution.vsize * j);
+			bmp.pos = (i + g_resolution.hsize * j);
 			pixel = (int *)(g_img->addr) + bmp.pos;
 			write(bmp.fd, pixel, 4);
 			i++;
