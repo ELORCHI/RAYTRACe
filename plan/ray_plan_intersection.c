d088f2b630ad2f6925482019db39c826c8f492b5
ELORCHI <elorchie@gmail.com>
2021-02-21 18:15:10 +0100

    normaliz plan

--- plan/ray_plan_intersection.c
+++ plan/ray_plan_intersection.c
@@ -13,42 +13,25 @@
 #include "../include/ray.h"
 #include "../include/plan.h"
 #include <stdio.h>
-//thiis code is found on stuck_overflow
 
-/*float denom = normal.dot(ray.direction);
-
-if (denom > 0)
+void			fill_inter(t_intersection *in, t_plan *pl, t_ray ray, float t)
 {
-	float t = -((center - ray.origin).dot(normal)) / denom;
-
-	if (t >= 0)
-	{
-		rec.tHit = t;
-		rec.anyHit = true;
-		computeSurfaceHitFields(ray, rec);
-		return true;
-	}
+	in->color = pl->color;
+	in->point = position(ray, t);
+	in->hit = t;
+	in->normal = normaliz(pl->normal);
 }
-
-float denom = normal.dot(ray.direction);
-if (abs(denom) > 0.0001f) // your favorite epsilon
-{
-	float t = (center - ray.origin).dot(normal) / denom;
-	if (t >= 0) return true; // you might want to allow an epsilon here too
-}
-return false;*/
 
 t_intersection	ray_plans_intersection(t_plan *plan, t_ray ray)
 {
-	float 			denom;
+	float			denom;
 	float			nom;
 	t_vector		origin_point;
-	t_intersection 	inter;
+	t_intersection	inter;
 	t_plan			*tmp_plan;
 
 	tmp_plan = plan;
 	inter.hit = FLT_MAX;
-
 	while (tmp_plan != NULL)
 	{
 		tmp_plan->normal = normaliz(tmp_plan->normal);
@@ -59,12 +42,7 @@ t_intersection	ray_plans_intersection(t_plan *plan, t_ray ray)
 			nom = dot_product(normaliz(tmp_plan->normal), origin_point) / denom;
 			nom *= -1;
 			if (nom > EPSILON && nom < inter.hit)
-			{
-				inter.color = tmp_plan->color;
-				inter.hit =  nom;
-				inter.point  = position(ray, nom);
-				inter.normal = normaliz(tmp_plan->normal);
-			}
+				fill_inter(&inter, tmp_plan, ray, nom);
 		}
 		tmp_plan = tmp_plan->next;
 	}
